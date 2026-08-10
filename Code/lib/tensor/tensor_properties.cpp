2e1ff0ea9c875fe0eae8ef5eeece3c8d7f839ab5
mohamedelhofy <alhofymohamed@gmail.com>
2026-08-10 10:46:50 +0300

    implement matrix-style formatted output for 1D and 2D tensors

--- Code/lib/tensor/tensor_properties.cpp
+++ Code/lib/tensor/tensor_properties.cpp
@@ -1,22 +1,55 @@
 #include "tensor/tensor.h"
 #include <iostream>
-
+#include <iomanip>
 std::ostream& operator<<(std::ostream& os, const Tensor& tensor) {
-    os << "Tensor(shape=[";
-    for (size_t i = 0; i < tensor.shape().size(); ++i) {
-        os << tensor.shape()[i];
-        if (i < tensor.shape().size() - 1) {
-            os << ", ";
-        }
+if (tensor.data().empty()) {
+        os << "Tensor([])";
+        return os;
     }
-    os << "], data=[";
-    for (size_t i = 0; i < tensor.data().size(); ++i) {
-        os << tensor.data()[i];
-        if (i < tensor.data().size() - 1) {
-            os << ", ";
+
+    if (tensor.ndim() == 1) {
+        os << "Tensor([";
+        for (size_t i = 0; i < tensor.shape()[0]; ++i) {
+            os << std::defaultfloat << std::setprecision(4) << tensor.data()[i];
+            if (i < tensor.shape()[0] - 1) {
+                os << ", ";
+            }
         }
+        os << "])";
+    } 
+    else if (tensor.ndim() == 2) {
+        size_t rows = tensor.shape()[0];
+        size_t cols = tensor.shape()[1];
+        
+        os << "Tensor([\n";
+        for (size_t i = 0; i < rows; ++i) {
+            os << "  [";
+            for (size_t j = 0; j < cols; ++j) {
+                os << std::setw(9) << std::fixed << std::setprecision(4) 
+                   << tensor.data()[i * cols + j];
+                
+                if (j < cols - 1) {
+                    os << ", ";
+                }
+            }
+            os << "]";
+            if (i < rows - 1) {
+                os << ",\n";
+            }
+        }
+        os << "\n])";
+    } 
+    else {
+        os << "Tensor(shape=[";
+        for (size_t i = 0; i < tensor.shape().size(); ++i) {
+            os << tensor.shape()[i];
+            if (i < tensor.shape().size() - 1) {
+                os << ", ";
+            }
+        }
+        os << "], data=...)"; 
     }
-    os << "])";
+    
     return os;
 }
 
