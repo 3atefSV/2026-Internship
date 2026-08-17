f0fc582f515b26b851b6a512d015f5c5051745e3
Abdelhamed Ahmed <168166326+Eng-abdelhamed@users.noreply.github.com>
2026-08-18 02:16:04 +0300

    Update binary_cross_entropy_loss.cpp

--- Code/lib/losses/binary_cross_entropy_loss.cpp
+++ Code/lib/losses/binary_cross_entropy_loss.cpp
@@ -46,6 +46,6 @@ void BinaryCrossEntropyLoss::check_in_unit_interval(const Tensor& tensor, std::s
         throw std::invalid_argument(std::string(name) +
                                     " must all lie between 0 and 1. "
                                     "Found min: " +
-                                    std::to_string(min_val) + ", max: " + std::to_string(max_val));
+                                    std::to_string(min_value) + ", max: " + std::to_string(max_value));
     }
 }
